e8b98cc9d1f77d0bf4aa2a611a1b19816949fe8b
MarcSiggelkow <marc.siggelkow@htw-dresden.de>
2023-02-02 16:11:50 +0100

    pictures

--- config_example.php
+++ config_example.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","d03a143f","ZXftqJqPNtoEBmV5","d03a143f");
+$con = mysqli_connect("localhost","user","password","db_name");
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error() ." | Seems like you haven't created the DATABASE with an exact name";
